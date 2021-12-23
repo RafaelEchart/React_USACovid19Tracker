8216b03269e45dbfc3fee169ccb7c3d78002c3d3
Rafael Echart <rafaelechartsaltos@gmail.com>
2021-12-23 01:10:26 -0500

    Favorite feature using localStorage

--- src/components/SelectedCountryHeader/SelectedCountryHeader.js
+++ src/components/SelectedCountryHeader/SelectedCountryHeader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './selectedCountryHeader.css';
 import { IoChevronBack } from 'react-icons/io5';
 import { AiFillHeart } from 'react-icons/ai';
@@ -6,7 +6,36 @@ import { Link } from 'react-router-dom';
 import usSeal from '../../assets/unitedStatesSeal.png';
 
 const SelectedCountryHeader = (props) => {
+  const [favorite, setFavorite] = useState(false);
   const { stateName, seal, website } = props;
+
+  useEffect(() => {
+    const favoriteList = JSON.parse(localStorage.getItem('favoriteStates'));
+    if (favoriteList && favoriteList.length) {
+      const isItFavorite = favoriteList.filter((fav) => fav === stateName);
+      if (isItFavorite.length) {
+        setFavorite(true);
+      }
+    }
+  }, []);
+
+  const handleFavoriteStates = () => {
+    const favoriteList = JSON.parse(localStorage.getItem('favoriteStates'));
+
+    if (favorite === false) {
+      if (favoriteList && favoriteList.length) {
+        favoriteList.push(stateName);
+        localStorage.setItem('favoriteStates', JSON.stringify(favoriteList));
+      } else {
+        localStorage.setItem('favoriteStates', JSON.stringify([stateName]));
+      }
+    } else if (favoriteList && favoriteList.length) {
+      const newFavorites = favoriteList.filter((fav) => fav !== stateName);
+      localStorage.setItem('favoriteStates', JSON.stringify(newFavorites));
+    }
+    setFavorite(!favorite);
+  };
+
   return (
     <header className="header">
       <Link to='/homePage'>
@@ -16,7 +45,7 @@ const SelectedCountryHeader = (props) => {
       <div className="SelectedCountryTitle"><img width={35} src={seal || usSeal} />{stateName}</div>
       </a>
       <div>
-          <AiFillHeart className="heart-icon" />
+          <AiFillHeart onClick={handleFavoriteStates} className={`heart-icon ${favorite ? 'favorite' : 'notfavorite'}`} />
       </div>
     </header>
   );
